ceb2939b44bee6b04c200ced7236a9c269e1d5dc
FomichevI <74508931+FomichevI@users.noreply.github.com>
2020-11-17 01:21:54 +0300

    Добавлена инерция движения
    Добавлена псевдоинерция, движения стали более плавными (регулируется настройками MoveSpeed, RotationSpeed и Drag самого объекта).
    Подобрал настройки, чтобы реально было не вылетать постоянно за пределы трассы.
    В скрипте перенес все из Update в FixedUpdate.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,13 +11,13 @@ public class PlayerController : MonoBehaviour
 
     public Transform zoidTransform;    
 
-    public Transform headTransform;   
-    
-    private bool ArrowControl; //Переменная для определения типа управления
+    public Transform headTransform;
 
-    private Vector3 position;
+    public Rigidbody rb;
 
-    private float rotationZ;   
+    private bool ArrowControl; //Переменная для определения типа управления 
+
+    private float rotationZ;    
 
 
     // Start is called before the first frame update
@@ -31,48 +31,44 @@ public class PlayerController : MonoBehaviour
         }else
         {
             ArrowControl = true;
-        }
-        position = transform.position;
+        }        
         rotationZ = transform.rotation.z;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (ArrowControl == false && Input.GetAxis("HorizontalP1") != 0)
         {
             rotationZ -= Input.GetAxis("HorizontalP1") * rotationSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, rotationZ);
         }
 
         if (ArrowControl == false && Input.GetAxis("VerticalP1") != 0)
         {
             if(Input.GetAxis("VerticalP1")<0)
-                position -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP1") * moveSpeed / 20;
+                rb.velocity -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP1") * moveSpeed / 2; //Скорость движения назад в 2 раза меньше, чем вперед
             else
-                position -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP1") * moveSpeed/10;
+                rb.velocity -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP1") * moveSpeed;
         }             
 
         if (ArrowControl == true && Input.GetAxis("HorizontalP2") != 0)
         {
             rotationZ -= Input.GetAxis("HorizontalP2") * rotationSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, rotationZ);
         }
 
         if (ArrowControl == true && Input.GetAxis("VerticalP2") != 0)
         {
             if (Input.GetAxis("VerticalP2") < 0)
-                position -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP2") * moveSpeed / 20;
+                rb.velocity -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP2") * moveSpeed / 2;
             else
-                position -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP2") * moveSpeed / 10;
+                rb.velocity -= (transform.position - headTransform.position) * Input.GetAxis("VerticalP2") * moveSpeed;
         }
 
 
 
     }
 
-    private void FixedUpdate()
-    {
-        transform.rotation = Quaternion.Euler(0, 0, rotationZ);
-        transform.position = position;
-    }
 
 }
 
