db4b124c06ab92c689f376d46183cd1aa60cd9b6
FomichevI <74508931+FomichevI@users.noreply.github.com>
2020-11-19 02:26:34 +0300

    Спавнер
    Создал спавнер. Пока что спавнит только бактерии. Пока что бактерии только движутся от одной точки до другой

---
+++ Assets/Scripts/BacteriumController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacteriumController : MonoBehaviour
+{   
+    public Transform finishTransform;
+
+    public float moveSpeed;
+
+    public float maxRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, finishTransform.position, moveSpeed);
+        if ((transform.position - finishTransform.position).magnitude<= maxRadius)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] baffSpawnPoints;
+
+    [Header ("Для точек с пометкой 1")] public Transform[] bacteriumSpawnPoints1;
+
+    [Header("Для точек с пометкой 2")] public Transform[] bacteriumSpawnPoints2;    
+
+    public GameObject bacteriumPrefab;
+
+    public float spawnBacteriumDelay = 5;
+
+    private float timeToSpawnBacterium;
+
+    void Start()
+    {
+        timeToSpawnBacterium = spawnBacteriumDelay;
+    }
+
+    private void FixedUpdate()
+    {
+        if (timeToSpawnBacterium > 0)
+            timeToSpawnBacterium -= 0.02f;
+        else
+        {
+            SpawnBacterium();
+            timeToSpawnBacterium = spawnBacteriumDelay;
+        }
+    }
+
+    private void SpawnBacterium()
+    {
+        int rand = Random.Range(0,2); // Для рандомизации точки начала движения
+        if (rand == 0)
+        {
+            rand = Random.Range(0, bacteriumSpawnPoints1.Length);
+            GameObject bact = Instantiate(bacteriumPrefab, bacteriumSpawnPoints1[rand]);
+            bact.GetComponent<BacteriumController>().finishTransform = bacteriumSpawnPoints2[rand];
+        }
+        else
+        {
+            rand = Random.Range(0, bacteriumSpawnPoints1.Length);
+            GameObject bact = Instantiate(bacteriumPrefab, bacteriumSpawnPoints2[rand]);
+            bact.GetComponent<BacteriumController>().finishTransform = bacteriumSpawnPoints1[rand];
+        }
+    }
+}
